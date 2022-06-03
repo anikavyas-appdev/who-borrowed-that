9d87bd3d7b1254e6be9df261ba7e0c6610d83e95
anikavyas <anikavyas@uchicago.edu>
2022-06-03 20:57:09 +0000

    finalized

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "items", :action => "items"})
+  get("/", { :controller => "items", :action => "index"})
 
   # Routes for the Borrower resource:
 
